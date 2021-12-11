e85fd1952221e3106039ef4db0eef95da75aff64
luisferhc <heringcoelho@gmail.com>
2021-12-10 21:01:22 -0300

    adicionando arquivo peças

---
+++ pecas.js
@@ -0,0 +1,41 @@
+
+var listadePecas = ["Filtro de Ar", "Bomba de Combustível", "Amortecedor"]
+    if(listadePecas.length < 10){
+        console.log("É possível cadastrar mais peças")
+    }else{
+        console.log("Não há mais espaço na lista")
+    }
+
+
+let peso = 50;
+    if(peso < 100){
+        console.log("A peça deve pesar no mínimo 100g");
+    }else{
+        console.log("A peça possui peso adequado");
+    }
+    
+    
+let nomePeca = "Disco de Freio"
+    if(nomePeca.length > 3){
+        console.log("Nome de peça está adequado para o cadastro!");
+    }else if(nomePeca.length == 0){
+        console.log("O nome da peça não pode ser vazio");
+    }else{
+        console.log("O nome deve ter mais de 3 caracteres, digite um nome adequado");
+    }
+
+
+    switch(nomePeca.length){
+    case 0:
+        console.log("O nome da peça não pode ser vazio");
+    break;
+
+    case 1:
+    case 2:
+    case 3:
+        console.log("O nome deve ter mais de 3 caracteres, digite um nome adequado");
+    break;
+
+    default:
+        console.log("O nome da peça está adequado");
+    }
